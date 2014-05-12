d13c014f0ab69aba892eea83bfce557fee5c5161
lijie.wong@gmail.com <lijie.wong@gmail.com>
2014-05-12 11:58:36 +1000

    Add tabbed_enh; modified setup.sh to link directories as well.

--- setup.sh
+++ setup.sh
@@ -7,24 +7,23 @@ then
   chsh -s /bin/zsh
 fi
 
-for file in $(find $(pwd)/dotfiles -maxdepth 1 -type f)
+for file in $(find $(pwd)/dotfiles -mindepth 1 -maxdepth 1)
 do
   target=$HOME/$(basename $file)
 
   if [ -e "$target" ]
   then
-    diff -q "$target" "$file" > /dev/null
-    if [ $? -ne 0 ]
+    if [ -L "$target" ]
     then
-      if [ -L "$target" ]
+      previous=$(readlink "$target")
+      if [ "$file" != "$previous" ]
       then
-        echo $(basename $file): overwriting link even though contents differ.
-      else
-        echo $(basename $file): skipping because of diffs.
-        continue
+        echo $(basename $file): Overwriting link previously pointing to $(readlink "$target").
       fi
+    else
+      echo $(basename $file): Skipping because target is a real file.
+      continue
     fi
   fi
-  rm -f $target
-  ln -s $file $target
+  ln -sf $file $target
 done
